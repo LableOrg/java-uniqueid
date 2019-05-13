e994533401570ec61417d9f10067dbe58e8f67e6
Jeroen Hoek <jeroen.hoek@lable.nl>
2019-05-13 09:41:01 +0200

    WIP

--- uniqueid-zookeeper/src/test/java/org/lable/oss/uniqueid/zookeeper/TempIT.java
+++ uniqueid-zookeeper/src/test/java/org/lable/oss/uniqueid/zookeeper/TempIT.java
@@ -20,26 +20,49 @@ import org.junit.Test;
 import org.lable.oss.dynamicconfig.zookeeper.MonitoringZookeeperConnection;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class TempIT {
     @Test
     @Ignore
-    public void test() throws IOException, InterruptedException {
+    public void test() throws IOException, InterruptedException, ExecutionException {
         MonitoringZookeeperConnection zookeeperConnection = new MonitoringZookeeperConnection("tzka,tzkb,tzkc");
 
-        for (int i = 0; i < 10; i++) {
-            ResourceClaim claim = ExpiringResourceClaim.claimExpiring(
-                    zookeeperConnection,
-                    256,
-                    "/unique-id-generator",
-                    TimeUnit.SECONDS.toMillis(30)
-            );
-            System.out.println(claim.get());
+        final int POOL_SIZE = 10;
+
+        ExecutorService pool = Executors.newFixedThreadPool(POOL_SIZE);
+        CompletionService<Integer> completionService = new ExecutorCompletionService<Integer>(pool);
+
+        for (int i = 0; i < POOL_SIZE; i++) {
+            completionService.submit(() -> {
+                try {
+                    ResourceClaim claim = ExpiringResourceClaim.claimExpiring(
+                            zookeeperConnection,
+                            256,
+                            "/unique-id-generator",
+                            TimeUnit.SECONDS.toMillis(30)
+                    );
+                    return claim.get();
+                } catch (IOException e) {
+                    return null;
+                }
+            });
         }
 
+        Set<Integer> ids = new HashSet<>();
+        for (int i = 0; i < POOL_SIZE; i++) {
+            Future<Integer> future = completionService.take();
+            ids.add(future.get());
+        }
 
         TimeUnit.SECONDS.sleep(35);
+
+        assertThat(ids.size(), is(POOL_SIZE));
     }
 
     @Test
